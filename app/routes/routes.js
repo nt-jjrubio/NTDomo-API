ecdde216c81035b309a3dc8d49da96695943ec51
Juan José Rubio <admin@tallernt.es>
2017-05-14 00:00:38 +0200

    Send and receive i2c command

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,15 +1,27 @@
 'use strict';
 
 const express = require('express');
-const api = express.Router()
+const api = express.Router();
 const testController = require('../controllers/testController.js');
 const deviceController = require('../controllers/deviceController.js');
+const i2cController = require('../controllers/i2cController.js');
+
 
 // API endpoints (All routes start with /api)
+
+// TODO: Remove all test endpoints
+/** All Test Endpoints **/
 api.get('/test', testController.testAPI);
 api.get('/test2/:name', testController.testAPIname);
 api.get('/error403', testController.testAPIerror);
+
+api.get('/testI2C/:dev', testController.testAPIi2c);
+
+
+/** Final endpoins **/
+// i2cRequest :dev <-- Device address, cmd <-- command
+api.get('/i2cRequest/:dev/:cmd', i2cController.i2cRequest);
+
 api.post('/device', deviceController.newDevice);
 api.get('/devices', deviceController.getDevices);
-api.get('/testI2C/:dev', testController.testAPIi2c);
 module.exports = api;
