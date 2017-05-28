3ac4611da76d2f87680a8fd8394507b75f4aa466
Juan José Rubio <admin@tallernt.es>
2017-05-28 11:31:44 +0200

    feture-i2c - Sketch arduino

--- sketch/wire/wire.ino
+++ sketch/wire/wire.ino
@@ -1,15 +1,42 @@
+/*******************************************************/
+/***** I2C Control - NTDomo - For arduino nano 328 *****/
+/*******************************************************/
+/* I2C Commands:                                       */
+/* ====================================================*/
+/* 0x02   -->  Return this device i2c address          */
+/* 0x10x  -->  ON, OFF pin X                           */
+/* 0x05x  -->  Return Digital PIN Status               */
+/* 0x04x  -->  Read analog X pin value                 */
+/*                                                     */
+/*******************************************************/
+
 #include <Wire.h>
 
-#define SLAVE_ADDRESS 0x60
+#define SLAVE_ADDRESS 0x09
 
 byte x = 0x00;
-int v; 
+
+bool pinState[8];
+
+int v;
 void setup()
 {
-  Wire.begin(0x08);
+  Wire.begin(SLAVE_ADDRESS);
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
   Serial.begin(9600);
+  Serial.print("Arduino nano with i2c address ");
+  Serial.print(SLAVE_ADDRESS);
+
+  // Initialize digital pins to off and set pin on digital mode
+  int i;
+  for( i = 0; i < 8; i++ )
+  {
+    pinState[i]=false;
+    pinMode(i, OUTPUT);
+  }
+
+
 }
 
 void loop()
@@ -25,13 +52,72 @@ void requestEvent()
 
   // Check request value from Master and send response (if request is 0x64 Write 200, else 0x65 response analog read)
   switch (x) {
-    case 100:
-      Wire.write(200);
+
+    // Whoami
+    case 2:
+      Wire.write(SLAVE_ADDRESS);
       break;
-    case 101:
-      v = analogRead(0);
-      Wire.write(v);
+
+    // TurnON and TurnOFF Digital pins
+    case 102:
+      onOffDigital(2);
       break;
+    case 103:
+      onOffDigital(3);
+      break;
+    case 104:
+      onOffDigital(4);
+      break;
+    case 105:
+      onOffDigital(5);
+      break;
+    case 106:
+      onOffDigital(6);
+      break;
+    case 107:
+      onOffDigital(7);
+      break;
+    case 108:
+      onOffDigital(8);
+      break;
+    case 109:
+      onOffDigital(9);
+      break;
+
+    // Read Digital PIN Status
+    case 52:
+      getPinStatus(2);
+      break;
+    case 53:
+      getPinStatus(3);
+      break;
+    case 54:
+      getPinStatus(4);
+      break;
+    case 55:
+      getPinStatus(5);
+      break;
+    case 56:
+      getPinStatus(6);
+      break;
+    case 57:
+      getPinStatus(7);
+      break;
+    case 58:
+      getPinStatus(8);
+      break;
+    case 59:
+      getPinStatus(9);
+      break;
+
+
+//    case 100:
+//      Wire.write(200);
+//      break;
+//    case 201:
+//      v = analogRead(0);
+//      Wire.write(v);
+//      break;
     default:
       Wire.write(x);
   }
@@ -50,4 +136,39 @@ void receiveEvent(int bytes)
       Serial.print("\n");
     }
   }
+}
+
+
+void onOffDigital (int pin){
+  if(pinState[pin-1]){
+    Serial.print("Off pin ");
+    Serial.print(pin);
+    digitalWrite(pin,LOW);
+    // 1xx Status codes ... 100 off 101 on
+    pinState[pin-1] = false;
+    Wire.write(0);
+
+  } else {
+
+    Serial.print("ON pin ");
+    Serial.print(pin);
+    digitalWrite(pin,HIGH);
+    pinState[pin-1] = true;
+    Wire.write(1);
+  }
+}
+
+void getPinStatus(int pin){
+  if(pinState[pin-1])
+  {
+    Wire.write(0x01);
+  } else {
+    Wire.write(0x00);
+  }
+}
+
+
+void getAnalogValue(int pin){
+  int v = analogRead(pin);
+  Wire.write(v);
 }
